36f2da6cbf64f64aa1814916d0c2373618df6f1d
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-07-17 06:09:46 +0100

    add hero section text

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -29,8 +29,16 @@ const Hero = () => {
   };
 
   return (
-    <div className="h-screen flex flex-col md:flex-row w-full">
-      <div className="bg-red-500 w-1/2">text</div>
+    <div className="md:h-screen flex flex-col md:flex-row w-full">
+      <div className="bg-gray-100 w-7/12 flex flex-col p-7 items-start justify-center gap-3">
+        <h1 className="text-4xl font-medium">Prestations Agricoles</h1>
+        <p className="opacity-75">
+          Analyse, Prestation, Travaux de sol & Conseils techniques.
+        </p>
+        <button className="bg-green-600 rounded-full px-5 py-2 text-gray-100 font-medium uppercase shadow-xl hover:translate-y-1 hover:shadow-none transition-all mt-4">
+          En Savoir plus
+        </button>
+      </div>
       <div className="w-full relative">
         <img
           className="absolute inset-0 w-full h-full object-cover"

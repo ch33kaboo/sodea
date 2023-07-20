2fa9e06e87eb82fa15c42438c5df01fc655e9779
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-07-20 09:27:23 +0100

    fix UI things in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,9 +3,9 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <div className="navbar bg-transparent absolute top-0 left-0 right-0 z-[9999]">
+    <div className="navbar bg-transparent absolute top-0 left-0 right-0 z-[9999] px-7">
       <div className="navbar-start flex flex-row gap-20 items-center justify-start">
-        <div>
+        <div className="flex justify-center items-center">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
               <svg
@@ -51,11 +51,15 @@ export default function Navbar() {
         <div className="hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
             <li>
-              <a className="text-gray-900 text-base">Item 1</a>
+              <a className="text-gray-900 hover:text-gray-900 text-base">
+                Item 1
+              </a>
             </li>
             <li tabIndex={0}>
               <details>
-                <summary className="text-gray-900 text-base">Parent</summary>
+                <summary className="text-gray-900 hover:text-gray-900 text-base">
+                  Parent
+                </summary>
                 <ul className="p-2">
                   <li>
                     <a>Submenu 1</a>
@@ -67,15 +71,20 @@ export default function Navbar() {
               </details>
             </li>
             <li>
-              <a className="text-gray-900 text-base">Item 3</a>
+              <a className="text-gray-900 hover:text-gray-900 text-base">
+                Item 3
+              </a>
             </li>
           </ul>
         </div>
       </div>
       <div className="navbar-end">
-        <button className="bg-green-600 rounded-full px-5 py-2 text-gray-100 font-medium uppercase shadow-xl hover:translate-y-1 hover:shadow-none transition-all mt-4">
-          Contactez-nous
-        </button>
+        <div className="relative">
+          <div className="absolute -inset-2 bg-gradient-to-r from-green-100 to-blue-100 rounded-full blur-md"></div>
+          <button className="relative bg-green-600 rounded-full px-5 py-2 text-gray-100 font-medium capitalize">
+            Contactez nous
+          </button>
+        </div>
       </div>
     </div>
   );

9aacf19f0a10ed70af423c9da701d54069c6eca8
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-07-22 23:37:44 +0100

    customize tailwind element's tabs to fit our UI

--- components/landing/products/Products.tsx
+++ components/landing/products/Products.tsx
@@ -9,16 +9,15 @@ const MyTabs = () => {
   }, []);
 
   return (
-    <div>
+    <div className="bg-gradient-to-b from-green-100 to-green-200 w-full px-16">
       <ul
-        className="mb-5 flex list-none flex-row flex-wrap border-b-0 pl-0"
+        className="mb-5 flex flex-row border-b-0"
         role="tablist"
         data-te-nav-ref
       >
         <li role="presentation" className="flex-auto text-center">
           <a
-            href="#tabs-home01"
-            className="my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate focus:border-transparent data-[te-nav-active]:border-primary data-[te-nav-active]:text-primary dark:text-neutral-400 dark:hover:bg-transparent dark:data-[te-nav-active]:border-primary-400 dark:data-[te-nav-active]:text-primary-400"
+            className="hover:bg-gray-900 hover:bg-opacity-5 hover:text-gray-950 transition-all select-none cursor-pointer my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-sm font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent focus:isolate focus:border-transparent data-[te-nav-active]:border-primary data-[te-nav-active]:text-primary"
             data-te-toggle="pill"
             data-te-target="#tabs-home01"
             data-te-nav-active
@@ -31,8 +30,7 @@ const MyTabs = () => {
         </li>
         <li role="presentation" className="flex-auto text-center">
           <a
-            href="#tabs-profile01"
-            className="focus:border-transparen my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate data-[te-nav-active]:border-primary data-[te-nav-active]:text-primary dark:text-neutral-400 dark:hover:bg-transparent dark:data-[te-nav-active]:border-primary-400 dark:data-[te-nav-active]:text-primary-400"
+            className="hover:bg-gray-900 hover:bg-opacity-5 hover:text-gray-950 transition-all select-none cursor-pointer focus:border-transparen my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-sm font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent focus:isolate data-[te-nav-active]:border-primary data-[te-nav-active]:text-primary"
             data-te-toggle="pill"
             data-te-target="#tabs-profile01"
             role="tab"
@@ -44,8 +42,7 @@ const MyTabs = () => {
         </li>
         <li role="presentation" className="flex-auto text-center">
           <a
-            href="#tabs-messages01"
-            className="my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate focus:border-transparent data-[te-nav-active]:border-primary data-[te-nav-active]:text-primary dark:text-neutral-400 dark:hover:bg-transparent dark:data-[te-nav-active]:border-primary-400 dark:data-[te-nav-active]:text-primary-400"
+            className="hover:bg-gray-900 hover:bg-opacity-5 hover:text-gray-950 transition-all select-none cursor-pointer my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-sm font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent focus:isolate focus:border-transparent data-[te-nav-active]:border-primary data-[te-nav-active]:text-primary"
             data-te-toggle="pill"
             data-te-target="#tabs-messages01"
             role="tab"
@@ -53,19 +50,6 @@ const MyTabs = () => {
             aria-selected="false"
           >
             Messages
-          </a>
-        </li>
-        <li role="presentation" className="flex-auto text-center">
-          <a
-            href="#tabs-contact01"
-            className="disabled pointer-events-none my-2 block border-x-0 border-b-2 border-t-0 border-transparent bg-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-400 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate focus:border-transparent dark:text-neutral-600"
-            data-te-toggle="pill"
-            data-te-target="#tabs-contact01"
-            role="tab"
-            aria-controls="tabs-contact01"
-            aria-selected="false"
-          >
-            Contact
           </a>
         </li>
       </ul>
@@ -95,14 +79,6 @@ const MyTabs = () => {
           aria-labelledby="tabs-profile-tab01"
         >
           Tab 3 content
-        </div>
-        <div
-          className="hidden opacity-0 transition-opacity duration-150 ease-linear data-[te-tab-active]:block"
-          id="tabs-contact01"
-          role="tabpanel"
-          aria-labelledby="tabs-contact-tab01"
-        >
-          Tab 4 content
         </div>
       </div>
     </div>

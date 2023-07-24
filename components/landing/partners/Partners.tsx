1aae1bf5dc2f28c64c857101e9c26f08da8b9cdf
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-07-24 10:40:21 +0100

    start working on partners section

---
+++ components/landing/partners/Partners.tsx
@@ -0,0 +1,60 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+
+export default function Partners() {
+  return (
+    <div className="bg-gradient-to-b from-green-200 to-green-300 w-full px-16 text-gray-900 flex flex-col items-center justify-center text-center py-16">
+      <h2 className="text-4xl font-bold font-poppins">Nos Partenaires</h2>
+      <p className="opacity-95">
+        Parmi les leaders mondiaux dans leurs domaines
+      </p>
+      <div className="flex flex-row">
+        <img
+          src="partnersLogos/brand_1.png"
+          alt="sodea partner"
+          className="w-72"
+        />
+        <img
+          src="partnersLogos/brand_2.png"
+          alt="sodea partner"
+          className="w-72"
+        />
+        <img
+          src="partnersLogos/brand_3.png"
+          alt="sodea partner"
+          className="w-72"
+        />
+        <img
+          src="partnersLogos/brand_4.png"
+          alt="sodea partner"
+          className="w-72"
+        />
+        <img
+          src="partnersLogos/brand_5.png"
+          alt="sodea partner"
+          className="w-72"
+        />
+        <img
+          src="partnersLogos/brand_6.png"
+          alt="sodea partner"
+          className="w-72"
+        />
+        <img
+          src="partnersLogos/brand_7.png"
+          alt="sodea partner"
+          className="w-72"
+        />
+        <img
+          src="partnersLogos/brand_8.png"
+          alt="sodea partner"
+          className="w-72"
+        />
+        <img
+          src="partnersLogos/brand_9.png"
+          alt="sodea partner"
+          className="w-72"
+        />
+      </div>
+    </div>
+  );
+}

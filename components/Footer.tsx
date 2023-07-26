3c854b4b28f484d77a1de8fc2927b37fecea9844
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-07-26 14:23:19 +0100

    start working on footer

---
+++ components/Footer.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="flex flex-row justify-between px-16 py-24 w-full font-sans">
+      <div className="flex flex-col items-start justify-center">
+        <div>hello</div>
+        <div>there</div>
+        <div>whatup</div>
+      </div>
+      <div className="flex flex-col items-start justify-center">
+        <div>hello</div>
+        <div>there</div>
+        <div>whatup</div>
+      </div>
+      <div className="flex flex-col items-start justify-center">
+        <div>hello</div>
+        <div>there</div>
+        <div>whatup</div>
+      </div>
+    </div>
+  );
+}

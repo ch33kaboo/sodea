9f7199a1743d31fd555f652db1a4e1508117e471
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-07-26 07:14:17 +0100

    make partners section, still not done + has some issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Hero from "@/components/landing/hero/Hero";
-import Partners from "@/components/landing/partners/Partners";
+// import Partners from "@/components/landing/partners/Partners";
 import Products from "@/components/landing/products/Products";
 import Sectors from "@/components/landing/Sectors";
 import Navbar from "@/components/Navbar";
-import "tw-elements/dist/css/tw-elements.min.css";
+// import "tw-elements/dist/css/tw-elements.min.css";
 
 export default function Home() {
   return (
@@ -12,7 +12,7 @@ export default function Home() {
       <Hero></Hero>
       <Sectors></Sectors>
       <Products></Products>
-      <Partners></Partners>
+      {/* <Partners></Partners> */}
     </main>
     //   <main className="flex min-h-screen flex-col items-center justify-between p-24">
     //     <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">

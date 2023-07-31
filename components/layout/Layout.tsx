79a997f03ef35afbf4365a23875fe22017162b7e
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-07-31 14:19:18 +0100

    made layout component (for navbar & footer)

---
+++ components/layout/Layout.tsx
@@ -0,0 +1,18 @@
+// @ts-ignore
+
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+type LayoutProps = {
+    children: React.ReactNode;
+};
+
+export default function Layout({ children }: LayoutProps) {
+    return (
+        <main className="flex flex-col items-center justify-start relative font-sans min-h-screen bg-gray-100">
+            <Navbar />
+            {children}
+            <Footer />
+        </main>
+    );
+}

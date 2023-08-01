c1fcee356781a753939b56f0e1015edae00024be
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-08-01 13:17:01 +0100

    minor product-schema change

--- sanity/schemas/product-schema.ts
+++ sanity/schemas/product-schema.ts
@@ -12,11 +12,23 @@ const productSchema = {
             name: "type",
             type: "string",
             title: "Type",
+            options: {
+                list: [
+                    { title: "Type 1", value: "type1" },
+                    { title: "Type 2", value: "type2" },
+                    { title: "Type 3", value: "type3" },
+                ],
+            },
         },
         {
             name: "image",
             type: "image",
             title: "Image",
+        },
+        {
+            name: "description",
+            type: "string",
+            title: "Description",
         },
     ],
 };

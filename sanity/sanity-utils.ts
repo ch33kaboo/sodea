4fef99c474c5ee4e7f7e047da9b6dc1f6f4ee402
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-08-01 07:36:15 +0100

    make calls with groq and getting it on the client side

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -10,7 +10,7 @@ export async function getProducts() {
     });
 
     return client.fetch(
-        groq`*[_type == "productSchema"]{
+        groq`*[_type == "product"]{
             _id,
             _createdAt,
             name,

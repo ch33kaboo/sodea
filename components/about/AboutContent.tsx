1a09da812916e457b81658d86556187aee97e28f
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-08-14 10:13:43 +0100

    working on about page

--- components/about/AboutContent.tsx
+++ components/about/AboutContent.tsx
@@ -1,5 +1,23 @@
 import React from "react";
 
 export default function AboutContent() {
-    return <div className="h-48 bg-pink-700">AboutContent</div>;
+    return (
+        <div className="w-full flex flex-col gap-20 px-[160px] my-24">
+            <p className="text-center text-gray-950 font-semibold">
+                Le Groupe Des Sociétés HASNAOUI N’a Cessé Depuis Sa Création De
+                Relever Le Pari De L’intégration Industrielle Avec Comme Slogan
+                « Construire Algérien ».
+            </p>
+            <div>
+                Le Groupe Des Sociétés HASNAOUI N’a Cessé Depuis Sa Création De
+                Relever Le Pari De L’intégration Industrielle Avec Comme Slogan
+                « Construire Algérien ».
+            </div>
+            <div>
+                Le Groupe Des Sociétés HASNAOUI N’a Cessé Depuis Sa Création De
+                Relever Le Pari De L’intégration Industrielle Avec Comme Slogan
+                « Construire Algérien ».
+            </div>
+        </div>
+    );
 }

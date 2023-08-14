b721b9a3e135ba16a84127570519fc03f02b1795
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-08-14 15:24:29 +0100

    used groq response in the page

--- components/about/AboutContent.tsx
+++ components/about/AboutContent.tsx
@@ -8,67 +8,38 @@ export default async function AboutContent() {
     let about = await getAboutContent();
     return (
         <div className="w-full flex flex-col gap-28 px-[160px] my-28">
-            <p
-                className="text-center text-gray-950 font-semibold"
-                onClick={() => {
-                    console.log(about[0]);
-                }}
-            >
-                Le Groupe Des Sociétés HASNAOUI N’a Cessé Depuis Sa Création De
-                Relever Le Pari De L’intégration Industrielle Avec Comme Slogan
-                « Construire Algérien ».
+            <p className="text-center text-gray-950 font-semibold">
+                {about[0].description_header}
             </p>
             <div className="flex flex-row gap-9">
                 <p className="text-gray-950 font-thin opacity-90">
-                    Notre société se lance en 1999 dans la production et mise à
-                    terre des plants maraîchers, plants francs ou greffer,
-                    arboricole, forestiers et d’ornement grâce à la
-                    modernisation des techniques acquises et son savoir-faire
-                    afin de répondre à la satisfaction des besoins et exigences
-                    des producteurs et agriculteurs.<br></br>
-                    <br></br>Dans son département de machinisme agricole, SODEA
-                    Spa apporte toute sa compétence dans la mécanisation afin de
-                    réduire les coûts et facilité le quotidien de ses
-                    collaborateurs et producteurs agricoles.<br></br>
-                    <br></br>SODEA SPA par sa solution globale est sa
-                    modernisation de l’activité a pour objectif d’accompagnée
-                    ses clients sur tout le processus hydraulique et
-                    environnemental, production et qualité des plants, systèmes
-                    d’irrigation et machines agricoles. Outre l’amélioration et
-                    l’efficacité productive, la mécanisation encourage la
-                    production à grande échelle et améliore en général la
-                    qualité des ressources produites.
+                    {about[0].first_section.first_sub_paragraphe}
+                    <br></br>
+                    <br></br>
+                    {about[0].first_section.second_sub_paragraphe}
+                    <br></br>
+                    <br></br>
+                    {about[0].first_section.third_sub_paragraphe}
                 </p>
                 <img
-                    src="about/about_sodea_1.jpg"
-                    className="w-[55%]"
+                    src={`${about[0].first_section.imageUrl}`}
+                    className="w-[55%] object-cover"
                     alt="about sodea"
                 />
             </div>
             <div className="flex flex-row-reverse gap-9">
                 <p className="text-gray-950 font-thin opacity-90">
-                    Notre société se lance en 1999 dans la production et mise à
-                    terre des plants maraîchers, plants francs ou greffer,
-                    arboricole, forestiers et d’ornement grâce à la
-                    modernisation des techniques acquises et son savoir-faire
-                    afin de répondre à la satisfaction des besoins et exigences
-                    des producteurs et agriculteurs.<br></br>
-                    <br></br>Dans son département de machinisme agricole, SODEA
-                    Spa apporte toute sa compétence dans la mécanisation afin de
-                    réduire les coûts et facilité le quotidien de ses
-                    collaborateurs et producteurs agricoles.<br></br>
-                    <br></br>SODEA SPA par sa solution globale est sa
-                    modernisation de l’activité a pour objectif d’accompagnée
-                    ses clients sur tout le processus hydraulique et
-                    environnemental, production et qualité des plants, systèmes
-                    d’irrigation et machines agricoles. Outre l’amélioration et
-                    l’efficacité productive, la mécanisation encourage la
-                    production à grande échelle et améliore en général la
-                    qualité des ressources produites.
+                    {about[0].second_section.first_sub_paragraphe}
+                    <br></br>
+                    <br></br>
+                    {about[0].second_section.second_sub_paragraphe}
+                    <br></br>
+                    <br></br>
+                    {about[0].second_section.third_sub_paragraphe}
                 </p>
                 <img
-                    src="about/about_sodea_2.jpg"
-                    className="w-[55%]"
+                    src={`${about[0].second_section.imageUrl}`}
+                    className="w-[55%] object-cover"
                     alt="about sodea"
                 />
             </div>

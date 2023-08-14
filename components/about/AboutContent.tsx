9a645896130d49787a0e31d72df81d74ad243af8
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-08-14 07:29:53 +0100

    structered the code for the new route

---
+++ components/about/AboutContent.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AboutContent() {
+    return <div className="h-48 bg-pink-700">AboutContent</div>;
+}
---
+++ components/about/Banner.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Banner() {
+    return <div className="h-48 bg-red-700">this is the banner</div>;
+}
